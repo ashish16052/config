899e78ad04203bf072365e03ef868360265644c4
ashish16052 <ashishmohite16052@gmail.com>
2025-06-11 11:46:25 +0530

    add angular lsp
    make sure to change angularls.lua acccording to
    `https://github.com/ashish16052/nvim-lspconfig`
    
    in the following path:
    `~/.local/share/nvim/lazy/nvim-lspconfig/lsp/angularls.lua`

--- init.lua
+++ init.lua
@@ -686,6 +686,7 @@ require("lazy").setup({
 				-- clangd = {},
 				-- gopls = {},
 				pyright = {},
+				angularls = {},
 				-- rust_analyzer = {},
 				-- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
 				--
