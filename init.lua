8c31cdb5fb0f108e3db0f7c22090596ca81c53b5
ashish16052 <ashishmohite16052@gmail.com>
2025-06-12 00:13:03 +0530

    add prettier formatter for ts/js/json

--- init.lua
+++ init.lua
@@ -786,6 +786,9 @@ require("lazy").setup({
 				--
 				-- You can use 'stop_after_first' to run the first available formatter from the list
 				-- javascript = { "prettierd", "prettier", stop_after_first = true },
+				typescript = { "prettier" },
+				javascript = { "prettier" },
+				json = { "prettier" },
 			},
 		},
 	},
